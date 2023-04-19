284226b2bfabcad482c5fb052a37e8f61e216af5
Prince Singhal <82727098+0xsYcamore@users.noreply.github.com>
2023-04-19 14:48:57 +0530

    Update Meta.sol

--- src/Meta.sol
+++ src/Meta.sol
@@ -1,10 +1,4 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.17;
-import {Test} from "forge-std/Test.sol";
-import {console} from "forge-std/console.sol";
-
-contract meta is Test{
+contract Meta is {
     
     address owner;
 
@@ -29,38 +23,8 @@ contract meta is Test{
             if iszero(extcodesize(contAddr)) { revert(0, 0) }
         }
         if(msg.sender.balance < 2 ether){
-            payable(msg.sender).transfer(0.1);
+            payable(msg.sender).transfer(0.1 ether);
         }
         return address(contAddr);
     }
-
-    function setUp() public {
-        address p = makeAddr("p");
-    }
-
-    function testone() public {
-        address x = deployMyContract(10, type(hmmm).creationCode, abi.encode(99, p));
-        console.log(x);
-
-    }
 }
-
-
-contract hmmm{
-    uint public a;
-    address public b;
-
-    constructor(uint _a, address _b){
-        a = _a;
-        b = _b;
-    }
-    function geta() public view returns(uint x){
-        x=a;
-    }
-
-    function getb() public view returns(address x){
-        x=b;
-    }
-}
-
-
